ac5db728feb193564f827b56767599afe749dbfc
Volodymyr Kolesnik <volodymyr_kolesnik@epam.com>
2019-12-03 14:36:16 +0200

    Initial Commit

---
+++ MVVM-C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Volodymyr Kolesnik on 12/3/19.
+//  Copyright © 2019 Volodymyr Kolesnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
